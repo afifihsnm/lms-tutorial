3dfeeaeed61cfc80d433137e91fe7918b099c840
afifihsnm <afifihsanke2@gmail.com>
2024-09-09 10:41:30 +0700

    feat: Intro & Demo - Course Attachment Form

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/attachments/route.ts
@@ -0,0 +1,10 @@
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    
+  } catch (error) {
+    console.log("COURSE_ID_ATTACHMENTS", error)
+  }
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider afterSignOutUrl="/">
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
